c5e1fec7cfd2b82afc6b5e19725ce42de26a7257
Erol Gurcan <59280972+erolgurcan@users.noreply.github.com>
2022-08-06 08:55:30 -0700

    Create stacks_queues.js

---
+++ stacks_queues.js
@@ -0,0 +1,16 @@
+// Run commands like push are being mostly used
+// Users can access the first and last elements
+// There are less actions than linked list and hash tables
+
+// Stacks => functions: lookup O(n), pop O(1), push O(1), peek O(1)
+//      LIFO: Last in first out
+//      Most languages have stack
+//      Example: Browser history, writing text and undo option
+
+//Queues => functions : lookup O(n) , enqueu O(1) , dequeue O(1) , peek O(1)
+//      FIFO: first in first out
+//      Frequently asked in interviews
+//      Example: Printer: sorts the task by order
+//      Inefficient, when remove first element shifting data
+
+
