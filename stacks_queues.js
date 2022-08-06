a8292b5f5c78f1877e93fe0dac3668f21b7ece60
Erol Gurcan <59280972+erolgurcan@users.noreply.github.com>
2022-08-06 10:49:05 -0700

    Update stacks_queues.js

--- stacks_queues.js
+++ stacks_queues.js
@@ -7,45 +7,66 @@
 //      Most languages have stack
 //      Example: Browser history, writing text and undo option
 
-
-
 //Stacks
 
 //Can be build by Arrays and Linked lists
 
-
 class Node {
-    constructor(value){
-        this.value = value;
-        this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.lenght = 0;
+  }
 
-    constructor() {
-        this.top = null;
+  peek() {
+    return this.top;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (this.lenght === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.lenght++;
+    return this;
+  }
+
+  pop() {
+    if (this.lenght === 0) {
+      return null;
+    } 
+    if (this.top === this.bottom){
         this.bottom = null;
-        this.lenght = 0;
     }
-
-    peek(){
-
-    }
-
-    push (value){
-
-    }
-
-    pop (){
-
-    }
+    this.top = this.top.next;
+    this.lenght--;
+    return this;
+  }
 }
 
-
 const myStack = new Stack();
-
-
+myStack.peek();
+myStack.push("google");
+myStack.push("udemy");
+myStack.push("discord");
+console.log(myStack);
+myStack.pop();
+console.log(myStack);
+myStack.pop();
+myStack.pop();
+console.log(myStack);
 
 //Queues => functions : lookup O(n) , enqueu O(1) , dequeue O(1) , peek O(1)
 //      FIFO: first in first out
@@ -53,9 +74,7 @@ const myStack = new Stack();
 //      Example: Printer: sorts the task by order
 //      Inefficient, when remove first element shifting data
 
-
 //Queues
 
 // Can be built by Arrays and Linked List
 // Linked List would better
-
