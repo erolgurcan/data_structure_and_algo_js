63b9c4c2f8dd6d2291f7d3654c883aa7a0c884a2
Erol Gurcan <59280972+erolgurcan@users.noreply.github.com>
2022-08-06 10:31:53 -0700

    Update stacks_queues.js

--- stacks_queues.js
+++ stacks_queues.js
@@ -7,10 +7,55 @@
 //      Most languages have stack
 //      Example: Browser history, writing text and undo option
 
+
+
+//Stacks
+
+//Can be build by Arrays and Linked lists
+
+
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.lenght = 0;
+    }
+
+    peek(){
+
+    }
+
+    push (value){
+
+    }
+
+    pop (){
+
+    }
+}
+
+
+const myStack = new Stack();
+
+
+
 //Queues => functions : lookup O(n) , enqueu O(1) , dequeue O(1) , peek O(1)
 //      FIFO: first in first out
 //      Frequently asked in interviews
 //      Example: Printer: sorts the task by order
 //      Inefficient, when remove first element shifting data
 
+
+//Queues
+
+// Can be built by Arrays and Linked List
+// Linked List would better
 
