bf9285a094a6856de41315c61b07da7c8a59e229
Erol Gurcan <59280972+erolgurcan@users.noreply.github.com>
2022-08-22 09:37:34 -0700

    Update algorithms_sorting.js

--- algorithms_sorting.js
+++ algorithms_sorting.js
@@ -18,6 +18,25 @@ function bubbleSort(array) {
 
 //Insertion Sort
 
+function insertionSort(array) {
+  const length = array.length;
+
+  for (let i = 0; i < length; i++) {
+    if (array[i] < array[0]) {
+      array.unshift(array.splice(i, 1)[0]);
+    } else {
+      for (let j = 1; j < i; j++) {
+        if (array[i] > array[j - 1] && array[i] < array[j]) {
+          array.splice(j, 0, array.splice(i, 1)[0]);
+        }
+      }
+    }
+  }
+
+  return array;
+}
+
+console.log(insertionSort(numbers));
 //Selection Sort
 
 function selectionSort(array) {
@@ -43,4 +62,6 @@ function selectionSort(array) {
 // console.log(selectionSort(numbers));
 
 //Merge Sort
+
+
 //Quick Sort
